3627b0046997a5eb10c65a2f6dc06396064633fe
WizMager <wizmager@gmail.com>
2022-08-28 19:51:45 +0300

    feat:fresh start task "Network" feat: add lobby login feat: add create room feat: add join room feat: add load game scene

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviourPunCallbacks
+{
+     [SerializeField] private Button leave;
+
+     private void Start()
+     {
+          leave.onClick.AddListener(LeaveRoom);
+     }
+
+     private void LeaveRoom()
+     {
+          PhotonNetwork.LeaveRoom();
+     }
+
+     public override void OnLeftRoom()
+     {
+          SceneManager.LoadScene(0);
+     }
+
+     public override void OnPlayerEnteredRoom(Player newPlayer)
+     {
+          Debug.Log($"Player{newPlayer} join to room");
+     }
+
+     public override void OnPlayerLeftRoom(Player otherPlayer)
+     {
+          Debug.Log($"Player{otherPlayer} left room");
+     }
+}
---
+++ Assets/Scripts/LobbyManager.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LobbyManager : MonoBehaviourPunCallbacks
+{
+    [SerializeField] private Button join;
+    [SerializeField] private Button create;
+    
+    private void Start()
+    {
+        if (PhotonNetwork.NetworkClientState != ClientState.ConnectingToMasterServer)
+        {
+            PhotonNetwork.AutomaticallySyncScene = true;
+            PhotonNetwork.ConnectUsingSettings(); 
+        }
+
+        join.onClick.AddListener(JoinRoom);
+        create.onClick.AddListener(CreateRoom);
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected To Master");
+    }
+
+    private void CreateRoom()
+    {
+        if (PhotonNetwork.NetworkClientState != ClientState.ConnectedToMasterServer) return;
+        PhotonNetwork.CreateRoom(null, new RoomOptions {MaxPlayers = 2});
+    }
+
+    private void JoinRoom()
+    {
+        if (PhotonNetwork.NetworkClientState != ClientState.ConnectedToMasterServer) return;
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        CreateRoom();
+    }
+
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.LoadLevel(1);
+    }
+
+    private void OnDestroy()
+    {
+        join.onClick.RemoveListener(JoinRoom);
+        create.onClick.RemoveListener(CreateRoom);
+    }
+}
